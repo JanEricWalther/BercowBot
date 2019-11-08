4f55ba85d1891646304fe50088cc6cd40b58454b
Jan Eric Walther <janeric.walther@stud.eah-jena.de>
2019-11-08 13:10:54 +0100

    added !commands-command; some cosmetic changes

--- index.js
+++ index.js
@@ -12,22 +12,23 @@ const sounds = './sounds/';
 let speakingMember = 0;
 
 // Array of Sound Files to randomly choose from 
-const soundFiles = ['ordah.mp3', 'Zen.mp3','ordah2.mp3','ordah3.mp3','ordah4.mp3','iKnowWhatImDoing.mp3','peopleShouting.mp3','theArtOfPatience.mp3','FlyingFlamingo.mp3'];
+const soundFiles = ['ordah.mp3', 'Zen.mp3', 'ordah2.mp3', 'ordah3.mp3', 'ordah4.mp3', 'iKnowWhatImDoing.mp3', 'peopleShouting.mp3', 'theArtOfPatience.mp3', 'FlyingFlamingo.mp3'];
 
 // gets run when bot has joined the discord server 
-bot.on('ready', async() => {
+bot.on('ready', async () => {
   console.log(`${bot.user.username} is online.`);
   bot.user.setActivity('Order in the House of Commons');
 });
 
-  
+// ON SPEAKING
+
 bot.on('guildMemberSpeaking', (member, speaking) => {
-          
-  if(speaking) {
-    console.log(`${member.user.username} started speaking`);
+
+  if (speaking) {
+    console.log(`${member.user.username} started speaking \n`);
   }
-  if(!speaking){
-    console.log(`${member.user.username} started speaking`);
+  if (!speaking) {
+    console.log(`${member.user.username} stopped speaking \n`);
   }
 });
 
@@ -35,8 +36,8 @@ bot.on('guildMemberSpeaking', (member, speaking) => {
 bot.on('message', async message => {
 
   // ignore message by the bot, in DMs, aswell as outside of the Server (Guild)
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
+  if (message.author.bot) return;
+  if (message.channel.type === 'dm') return;
   if (!message.guild) return;
 
   // Command Prefix set in botconfig.json
@@ -48,41 +49,72 @@ bot.on('message', async message => {
   let args = messageArray.slice(1);
 
 
-  // JOIN CHANNEL
-  if(command === `${prefix}join`) {
+  switch (command) {
+    case `${prefix}order`:
+      sendSoundFile(Math.round(Math.random() * soundFiles.length - 1), message);
+      break;
 
-    if(message.member.voiceChannel) {
+    case `${prefix}ordah`:
+      sendSoundFile(Math.round(Math.random() * soundFiles.length - 1), message);
+      break;
+
+    case `${prefix}zen`:
+      sendSoundFile(1, message);
+      break;
+
+    case `${prefix}flamingo`:
+      sendSoundFile(8, message);
+      break;
+
+    case `${prefix}botinfo`:
+      botInfo(message);
+      break;
+
+    case `${prefix}commands`:
+      commands(message);
+      break;
+
+    case `${prefix}join`:
       message.member.voiceChannel.join();
-    }
+      break;
+
+    case `${prefix}leave`:
+      message.member.voiceChannel.leave();
+      break;
   }
-
-  // BOTINFO
-  if(command === `${prefix}botinfo`){
-
-    let botIcon = bot.user.displayAvatarURL; // Avatar Icon
-    let botEmbed = new Discord.RichEmbed()
-    .setDescription('Bot Information')
-    .setColor('#eeeeee')    // stripe on the right
-    .setThumbnail(botIcon)
-    .addField('Bot Name', bot.user.username);
-
-    return message.channel.send(botEmbed);
-  }
-
-  // ORDER
-  // select random Sundfile from the Soundfile Array
-  if(command === `${prefix}order` || command === `${prefix}ordah`) sendSoundFile(Math.round(Math.random()*soundFiles.length-1), message);
-
-  // special command to zen the Zen Soundfile
-  if(command === `${prefix}zen`) sendSoundFile(1, message);
-
-  // special command to zen the Zen Soundfile
-  if(command === `${prefix}flamingo`) sendSoundFile(8, message);
 });
 
 bot.login(botconfig.token);
 
 
+// COMMANDS
+// BOTINFO
+function botInfo(message) {
+
+  let botIcon = bot.user.displayAvatarURL; // Avatar Icon
+  let botEmbed = new Discord.RichEmbed()
+    .setDescription('Bot Information')
+    .setColor('#eeeeee')    // stripe on the right
+    .setThumbnail(botIcon)
+    .addField('Bot Name', bot.user.username)
+    .addField('Info', `Use ${botconfig.prefix}commands to get a List of available Commands`);
+
+  return message.channel.send(botEmbed);
+}
+
+// COMMANDS
+function commands(message) {
+
+  let botEmbed = new Discord.RichEmbed()
+    .setDescription('Bot Commands')
+    .setColor('#eeeeee')    // stripe on the right
+    .addField(`${botconfig.prefix}order / ${botconfig.prefix}ordah`, 'For when you are in need of some Ordah.')
+    .addField(`${botconfig.prefix}zen`, 'For when you need some Zen in your life.')
+    .addField(`${botconfig.prefix}flamingo`, `For when you don't give a flying Falmingo.`)
+    .addField(`${botconfig.prefix}botinfo`, `Bot Info`);
+
+  return message.channel.send(botEmbed);
+}
 
 // Sends certain soundfile of the soundArray in the Channel of a user that called the bot 
 function sendSoundFile(arrayIndex, message) {
@@ -95,7 +127,7 @@ function sendSoundFile(arrayIndex, message) {
         console.log(`playing ${soundFiles[arrayIndex]}\n`);
         // set up dispatcher, that plays the soundfile 
         const dispatcher = connection.playFile(sounds.concat(soundFiles[arrayIndex]));
-        
+
         // leave, after the soundfile has ended
         dispatcher.on('end', () => {
           message.member.voiceChannel.leave();
@@ -103,6 +135,6 @@ function sendSoundFile(arrayIndex, message) {
       })
       .catch(console.log);
   } else {
-    message.reply('You need to join a voice channel that is in need for some ORDAH!');
+    message.reply('You need to join a voice channel that is in need of some ORDAH!');
   }
 }
